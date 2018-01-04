4e42d05103086b6274f9888bf3ca120f395fdb24
Stephen Russell <stephenjohnrussell@me.com>
2018-01-04 16:11:24 +0000

    oh yeah, the main file hast to match the folder name

---
+++ car_control/car_control.ino
@@ -0,0 +1,14 @@
+// Set distance threshold to stop car in cm
+int DISTANCE_THRESHOLD = 5;
+
+void setup() {
+  // Ultrasound setup
+  Serial.begin(9600);
+  ultrasoundSetup();
+}
+
+void loop() {
+  if ObjectAhead() {
+
+  }
+}
